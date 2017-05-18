e1224c5fcb9d53af4b849d5da852c5362bc4b601
killmaster <carlosmartins8@gmail.com>
2017-05-18 09:41:53 +0100

    new stuff

--- .config/i3/naturalscroll.sh
+++ .config/i3/naturalscroll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in $(xinput list | grep "Sensei" | perl -n -e'/id=(\d+)/ && print "$1\n"'
-	xinput set-prop $i 286 0
+for i in $(xinput list | grep "Sensei" | perl -n -e'/id=(\d+)/ && print "$1\n"')
+	xinput set-prop "$i" 286 0
 done
